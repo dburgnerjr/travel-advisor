9eca46e77bd1c763e502e74a16ee203d8728471c
Daniel Burgner <dburgnerjr@yahoo.com>
2025-09-12 02:05:29 -0500

    Header buildout complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@mui/material';
-import { SearchIcon } from '@mui/icons-material';
+import SearchIcon from '@mui/icons-material/Search';
 
 import useStyles from './styles';
 
 const Header = () => {
     const classes = useStyles();
+
     return (
         <AppBar position="static">
             <Toolbar className={classes.toolbar}>
@@ -17,14 +18,14 @@ const Header = () => {
                     <Typography variant="h6" className={classes.title}>
                         Explore new places
                     </Typography>
-                    <Autocomplete>
+                    {/* <Autocomplete> */}
                         <div className={classes.search}>
                             <div className={classes.searchIcon}>
                                 <SearchIcon />
                             </div>
-                            <InputBase placeholder="Search ... " classes={{ root: classes.inputRoot, input: classes.inputInput}}/>
+                            <InputBase placeholder="Search ... " classes={{ root: classes.inputRoot, input: classes.inputInput}} />
                         </div>
-                    </Autocomplete>
+                    {/* </Autocomplete> */}
                 </Box>
             </Toolbar>
         </AppBar>
