96f5e7dcf9d9c83b9d1e97ee27a66bbedb63ba82
Daniel Burgner <dburgnerjr@yahoo.com>
2025-09-15 12:16:39 -0500

    Map buildout complete

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ const App = () => {
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
-          <Grid item xs={12} md={4}>
+          <Grid size={{ xs: 12, md: 4 }}>
             <List />
           </Grid>
-          <Grid item xs={12} md={8}>
+          <Grid size={{ xs: 12, md: 8 }}>
             <Map />
           </Grid>
       </Grid>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@mui/material';
+import { LocationOnOutlined } from '@mui/icons-material';
+import Rating from '@mui/material';
+
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0 };
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCSBI1qxePUu4jLU2HpwCYy4grV4b4m0BM' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margins={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
